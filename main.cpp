195b6122b24fc1a9cc3f8e8ac17a8bb11deccb30
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-05-24 14:47:23 +0530

    My first Namaste Duniya

---
+++ main.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+/*
+
+  int a;
+  cin>>a;
+  cout<< "Value of n is:"<< a <<endl;
+
+   // if a is positive 
+   /*
+   if (a>0) {
+    cout<< "A is posotive " << endl;
+   }
+  
+    else {
+        cout<< " A is negative " << endl;
+    }
+
+
+
+   int a,b;
+   
+   cout <<"Enter the value of A "<<endl;
+   cin>>a;
+
+   cout<< " Enter the value of B"<<endl;
+   cin>>b;
+
+   if(a>=b) {
+    cout<<" A is greater than B "<<endl;
+
+   }
+   
+   else {
+    cout << " B is greater than A"<<endl;
+
+   }
+    
+
+    int a ;
+    
+    cout << " Enter the value of A "<< endl;
+    cin>>a;
+
+    if(a>0) {
+        cout<< " The value of A is positive"<<endl;
+
+    }
+
+    else {
+
+        if(a<0) {
+            cout<< " The value of A is negative "<<endl;
+        }
+
+        else {
+            cout<<"The value of A is zero "<<endl;
+        }
+    }
+        
+
+        int a ;
+
+        cout<< " Enter the value of A"<<endl;
+        cin>>a;
+        
+
+        if (a>0) {
+            cout<< " A is positive"<<endl;
+        }
+        else if(a<0) { 
+            cout<< " A is negative"<<endl;\
+        }
+        else {
+            cout<<"A is 0 "<<endl;
+        }     
+            
+
+      int a = 9; 
+      if (a==9) {
+        cout<<"NINEY"<<endl;
+      }
+
+     if (a>0) {
+        cout<<"POSITIVE"<<endl;
+     }
+    else {
+        cout <<"NEGATIVE"<<endl;
+    }
+        
+
+        int a = 24;
+        if (a>20) {
+            cout<<"love"<<endl;
+        }
+        else if (a==24) {
+            cout<<"Lovely"<<endl;
+        }
+        else {
+            cout<< " Babber"<<endl;
+        }
+
+        cout<<a;
+        */
+       /*Home work question*/
+
+        char ch;
+
+        cout<<"Enter your question"<<endl;
+        cin>>ch;
+         
+        if(ch>='a' and ch<='z') { 
+        cout<< "This is lower case"<<endl;
+        }
+
+        if(ch>='A' and ch<='Z') {
+            cout<<"this is upper case"<<endl;
+        }
+
+        else if(ch>=1 and ch<=9){
+            cout<<"the answer is numeric"<<endl;
+        }
+
+        
+
+        
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ sneh.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+/*
+int main () {
+
+    int n;
+    cin>>n;
+    int i= 1;
+    while(i<=n) {
+        cout<<i<<endl;
+        i=i+1;
+    }
+}
+*/
+int main () {
+    /*
+    int n;
+    cin>>n;
+    int i = 1;
+    int sum = 0;
+
+    while(i<=n) {
+        
+        sum = sum + i;
+        i = i + 1;
+    }
+    
+    cout<<" Value of sum :"<<sum<<endl;
+
+*/
+/*
+    int n;
+    cin>>n;
+    int a = 2;
+    int sum = 0;
+
+    while(a<=n) {
+        sum = sum + a ;
+        a = a + 2 ;
+       
+        }
+        cout<<" Value of sum of even number :"<<sum<<endl;
+        
+        
+            
+
+        
+            int n;
+            cin>>n;
+            int i = 1;
+            int sum = 0;
+
+            while(i<=n) {
+                sum = sum + i;
+                i = i + 2;
+            }
+        cout<<" Sum of odd numbers :- "<<sum<<endl;  */
+
+        int n;
+        cin>>n;
+        int i = 2;
+        while(i<n) {
+            if(n%i == 0) { 
+            cout <<" Not prime "<< i <<endl;
+            }
+
+            else {
+                cout<<" Prime "<< i <<endl;
+            }
+
+            i= i+1;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
