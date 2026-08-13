0e9b8fb49ae33b86a0054918d3ed0a0dbe6ce6c3
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-08-14 00:21:22 +0530

    forncpp

---
+++ .vscode/forloop1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main ()  {
+    
+    int n;
+    cout<<"Enter the value of n "<<endl;
+    cin>>n;
+    cout<<" Printing the number from 1 to n "<<endl;
+    /*
+    int i;
+    i=1;
+    for(;i<=n;) {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+    
+   int n;
+    cout<<"Enter the value of n "<<endl;
+    cin>>n;
+    cout<<" Printing the number from 1 to n "<<endl;
+    int i;
+    i=1;
+    for(; ;) {
+        if(i<=n); {
+        cout<<i<<endl;
+        i++;
+    }
+        */
+
+    for(int a=0 , b=1; a<=0 && b>=1; a-- , b--) {
+        cout<<a<<" "<<b<<" "<<endl;
+        
+    } 
+}
