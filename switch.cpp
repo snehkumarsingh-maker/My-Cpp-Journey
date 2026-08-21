3c2ccaa68fda6bf011cbfb9b2281583e57e05172
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-08-21 05:57:15 +0530

    Switch

---
+++ switch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int num = 1;
+    char ch = '1';
+
+    switch(num) {
+        case 1: cout<<"First case"<<endl;
+                cout<<"First again"<<endl;
+                break;
+        case 2: cout<<"Second case"<<endl;
+                break;
+
+        default: cout<<"Default case"<<endl;
+        
+    }
+
+
+}
