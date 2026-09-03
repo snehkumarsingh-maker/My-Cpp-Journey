367b84b549aa9792e57341b76514df7b0f9fdc4a
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-03 23:37:21 +0530

    testcase

---
+++ Testcode.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std; 
+
+int firstOcc(int arr[], int n, int key) {
+  
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while(s <=e)    {
+
+        if (arr[mid] == key)    {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+
+}
+
+int lastOcc(int arr[], int n, int key) {
+  
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while(s <=e )    {
+
+        if (arr[mid] == key)    {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key > arr[mid]) {
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+
+}
+
+int main() {
+     int even[5] = {1, 2, 3, 4, 5};
+
+    cout<<"First Occurance of 3 is at index:"<< firstOcc(even, 5,3) <<endl;
+    cout<<"Last Occurance of 3 is at index:"<< lastOcc(even, 5,3) <<endl;
+}
