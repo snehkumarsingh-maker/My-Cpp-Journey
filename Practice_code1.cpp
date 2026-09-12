a5f37f38c79f0e5155d74137bbb60afcd9d698df
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-12 23:21:45 +0530

    practice code  1

---
+++ Practice_code1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    char ch[5] = {'a','b','c','r','p'};
+    cout<< ch[3]<<endl;
+    cout<< "Printing the array"<<endl;
+    //print the array
+    for (int i = 0; i < 5; i++) {
+    cout<< ch[i] << " ";
+    }
+    cout<< "Printing done";
+    
+    double firstdouble[5];
+    float firstfloat[6];
+    bool firstbool[9];
+
+
+
+}
