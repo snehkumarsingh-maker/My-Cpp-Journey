dba97a2753f940a3eeb61dd00940bbd55c58f93c
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-14 01:39:50 +0530

    Today work

--- Practice list folder.cpp/Practice_code1.cpp
+++ Practice list folder.cpp/Practice_code1.cpp
@@ -19,7 +19,65 @@ int third[15]= {4,9};
     int n3 = 15;
     Printarray(third,15);
 }
-*/
 
-void PrintArray(int arr[],int size)  {}
+
+void PrintArray(int arr[],int size)  {
+    cout << "printing the array " << endl;
+    //Printing the array 
+
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " " << endl;
+
+    }
+    cout << "Printing array"<<endl;
+}
+
+int main()  {
+    int sneh[23]= {3,2,3l,24,534,134, 34,1234, 23};
+    int m = 15;
+    PrintArray(sneh,m);
+
+    int Utkarsh[9]={1,2};
+    int n = 8;
+    PrintArray(Utkarsh,3);
+    
+
+}
+
+
+void PrintArray(int arr[],int size) {
+    cout << " Printing the array "<< endl;
+
+    for(int i = 1; i < size; i++)   {
+        cout << "arr[i]" <<" "<<endl;
+    }
+cout << "Printing Done" << endl;
+}
+
+int main()  {
+    int sneh[7]= {1,2,3,4,5,6,7};
+    int n = 1;
+    PrintArray(sneh,n);
+}
+*/
+void PrintArray(int arr[], int size)    {
+    cout << " Printing the array "<< endl;
+
+    for(int i = 0; i < size; i++)   {
+        cout << arr[i] <<endl;
+    }
+    cout<<"Printing Done"<<endl;
+
+}
+
+int main()  {
+    int sneh[2]= {1,2};
+    int n = 2;
+
+    PrintArray(sneh,n);
+
+    int fifth[10]={1};
+    n = 10;
+    PrintArray(fifth,n);
+}
 
