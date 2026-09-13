b9671d27ef56aa581074c2acf5fe00812ed98d66
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-13 11:56:35 +0530

    practice problem

---
+++ Practice_code2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//HOLD
+void printArray() {
+
+}
+
+int main()  {
+
+    //declare
+    int number[15];
+
+    //accesing an array
+    cout << " Value at 14 index " << number[14] << endl;
+    
+    cout << " Value at 20 index " << number[20] << endl;
+
+    cout << endl << "Everthing is Fine " << endl << endl;
+
+    return 0;
+}
