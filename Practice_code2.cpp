09ee7d73a5a9c0de9b22a8830fc34daa8db005bc
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-13 12:12:35 +0530

    Pattern

--- Practice_code2.cpp
+++ Practice_code2.cpp
@@ -6,7 +6,7 @@ void printArray() {
 
 }
 
-int main()  {
+int main()  { /*
 
     //declare
     int number[15];
@@ -19,4 +19,15 @@ int main()  {
     cout << endl << "Everthing is Fine " << endl << endl;
 
     return 0;
+
+    */
+
+    int num[3]={ 2, 89, 91 };
+    
+    cout << "value at 2nd Index " << num[2] << endl;
+
+    cout << "value at 0th index " << num[0] << endl;
+
+    cout << "Vallue at 1th Index " << num[1] << endl;
+
 }
