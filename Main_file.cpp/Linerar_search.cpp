8f4def7901eb9e277bc98ae19213cab39ec1a696
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-19 14:17:12 +0530

    Linear Search

---
+++ Main_file.cpp/Linerar_search.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+bool search(int arr[], int size, int key)   {
+
+    for (int i = 0; i < size; i++)  {
+        
+        if (arr[i] == key) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+int main() {
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int key;
+
+     cout << " Enter the key " << endl;
+     
+    cin>>key;
+
+   
+
+    bool found = search(arr , 10, key);
+
+    if (found) {
+        cout << "Key is persent"<< endl;
+
+    }
+    else {
+        cout << "Key is absent "<< endl;
+    }
+
+
+
+
+}
